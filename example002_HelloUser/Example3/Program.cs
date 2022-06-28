34db6c6b6d0e5d3dad7e54ffb7fb8efbc2786d9e
Vitaly <Vitaliy1999@mail.ru>
2022-06-28 11:54:42 +0600

    Делим с остатком

--- example002_HelloUser/Example3/Program.cs
+++ example002_HelloUser/Example3/Program.cs
@@ -1,5 +1,9 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿//int numberA = 3;
+//int numberB = 5;
 //Console.WriteLine(numberA + numberB)
-int result = (numberA + numberB);
-Console.WriteLine(result);
+//int result = (numberA + numberB);
+//Console.WriteLine(result);
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+//Console.WriteLine(result);
