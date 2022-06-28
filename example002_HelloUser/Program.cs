c0767325116c80a8946c30e82d8014e0e044b521
Vitaly <Vitaliy1999@mail.ru>
2022-06-28 11:30:01 +0600

    новая версия программы

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("введите Ваше имя");
+string username = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);    
+Console.Write("Привет, ");
+Console.Write(username);    
