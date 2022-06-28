19ed1c37b2f86a5705b59ad64fcde5ead9affb46
Vitaly <Vitaliy1999@mail.ru>
2022-06-28 12:51:59 +0600

    Случайные числа

--- example002_HelloUser/Example3/Program.cs
+++ example002_HelloUser/Example3/Program.cs
@@ -3,7 +3,14 @@
 //Console.WriteLine(numberA + numberB)
 //int result = (numberA + numberB);
 //Console.WriteLine(result);
-double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+//double numberA = 12;
+//double numberB = 5;
+//Console.WriteLine(numberA / numberB);
 //Console.WriteLine(result);
+int numberA = new Random().Next(1,10); // 1...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
+//Console.WriteLine(numberA + numberB)
